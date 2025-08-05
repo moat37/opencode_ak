643316a841e6102e0ab18847d3f481586c23ffd5
stri7er <ishaan.karanjkar@gmail.com>
2025-08-06 00:38:13 +0530

    6 Aug 12:30AM

--- packages/opencode/src/provider/transform.ts
+++ packages/opencode/src/provider/transform.ts
@@ -47,7 +47,7 @@ export namespace ProviderTransform {
   }
 
   export function temperature(_providerID: string, modelID: string) {
-    if (modelID.toLowerCase().includes("qwen")) return 0.4
+    if (modelID.toLowerCase().includes("qwen")) return 0.3
     return 0.3
   }
 }
