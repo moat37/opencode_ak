5fffafb130b9d5fb8ee7b37e5b44b35c0b544d51
stri7er <ishaan.karanjkar@gmail.com>
2025-08-12 19:13:41 +0530

    12 Aug 7:15PM - Removed unnecessary sanitization

--- packages/opencode/src/session/message-v2.ts
+++ packages/opencode/src/session/message-v2.ts
@@ -363,8 +363,9 @@ export namespace MessageV2 {
             {
               ...base,
               type: "tool",
+              callID: part.toolInvocation.toolCallId,
               // Added by Akshay. Sanitize toolCallId to ensure it matches the required pattern ^[a-zA-Z0-9_-]+$
-              callID: part.toolInvocation.toolCallId.replace(/[^a-zA-Z0-9_-]/g, '_'),
+              //callID: part.toolInvocation.toolCallId.replace(/[^a-zA-Z0-9_-]/g, '_'),
               tool: part.toolInvocation.toolName,
               state: (() => {
                 if (part.toolInvocation.state === "partial-call") {
@@ -508,14 +509,14 @@ export namespace MessageV2 {
               ]
             if (part.type === "tool") {
               // Added by Akshay. Sanitize toolCallId to ensure it matches the required pattern ^[a-zA-Z0-9_-]+$
-              const sanitizedToolCallId = part.callID.replace(/[^a-zA-Z0-9_-]/g, '_');
+              //const sanitizedToolCallId = part.callID.replace(/[^a-zA-Z0-9_-]/g, '_');
               if (part.state.status === "completed")
                 return [
                   {
                     type: ("tool-" + part.tool) as `tool-${string}`,
                     state: "output-available",
-                    //toolCallId: part.callID,
-                    toolCallId: sanitizedToolCallId, //Modified by Akshay
+                    toolCallId: part.callID,
+                    //toolCallId: sanitizedToolCallId, //Modified by Akshay
                     input: part.state.input,
                     output: part.state.output,
                   },
@@ -525,8 +526,8 @@ export namespace MessageV2 {
                   {
                     type: ("tool-" + part.tool) as `tool-${string}`,
                     state: "output-error",
-                    //toolCallId: part.callID,
-                    toolCallId: sanitizedToolCallId, //Modified by Akshay
+                    toolCallId: part.callID,
+                    //toolCallId: sanitizedToolCallId, //Modified by Akshay
                     input: part.state.input,
                     errorText: part.state.error,
                   },
